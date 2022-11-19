daf057a87c8d0cfda03ca20a8e86ac2f62d42f5e
jougarcia <jougarcia95@gmail.com>
2022-11-19 14:10:02 +0000

    joseph

--- src/app.js
+++ src/app.js
@@ -8,4 +8,25 @@ import "./assets/img/4geeks.ico";
 window.onload = function() {
   //write your code here
   console.log("Hello Rigo from the console!");
+
+  document.querySelector("#btn").addEventListener("click", generarDominios);
 };
+
+function generarDominios() {
+  let pronoun = ["the", "our"];
+  let adj = ["great", "big"];
+  let noun = ["jogger", "racoon"];
+
+  let dominios = "";
+
+  for (let i = 0; i < pronoun.length; i++) {
+    for (let j = 0; j < adj.length; j++) {
+      for (let k = 0; k < noun.length; k++) {
+        let dominio = pronoun[i] + adj[j] + noun[k] + ".com";
+        dominios =
+          dominios + '<li class ="list-group-item">' + dominio + "<li>";
+      }
+    }
+  }
+  document.getElementById("generarDominios").innerHTML = dominios;
+}
